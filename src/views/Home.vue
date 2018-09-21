216bec2d78b74215cdb4fd45e11a998e4b14077b
Jost Schulte <jost@jibble.io>
2018-09-21 09:36:29 +0200

    move css to separate file & add image

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+    <img alt="A" src="../assets/A.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
